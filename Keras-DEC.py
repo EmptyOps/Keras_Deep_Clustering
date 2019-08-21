504aae26886c5d03941444c38b0dfa789f74e9d5
hiren <hiren@emptyops.com>
2019-08-21 18:14:58 +0530

    python script

--- Keras-DEC.py
+++ Keras-DEC.py
@@ -15,8 +15,6 @@ from sklearn.cluster import KMeans
 import metrics
 
 
-
-
 def autoencoder(dims, act='relu', init='glorot_uniform'):
     """
     Fully connected auto-encoder model, symmetric.
@@ -515,36 +513,36 @@ batch_size = 256
 save_dir = './results'
 
 
-def autoencoder(dims, act='relu', init='glorot_uniform'):
-    """
-    Fully connected auto-encoder model, symmetric.
-    Arguments:
-        dims: list of number of units in each layer of encoder. dims[0] is input dim, dims[-1] is units in hidden layer.
-            The decoder is symmetric with encoder. So number of layers of the auto-encoder is 2*len(dims)-1
-        act: activation, not applied to Input, Hidden and Output layers
-    return:
-        (ae_model, encoder_model), Model of autoencoder and model of encoder
-    """
-    n_stacks = len(dims) - 1
-    # input
-    input_img = Input(shape=(dims[0],), name='input')
-    x = input_img
-    # internal layers in encoder
-    for i in range(n_stacks-1):
-        x = Dense(dims[i + 1], activation=act, kernel_initializer=init, name='encoder_%d' % i)(x)
+# def autoencoder(dims, act='relu', init='glorot_uniform'):
+#     """
+#     Fully connected auto-encoder model, symmetric.
+#     Arguments:
+#         dims: list of number of units in each layer of encoder. dims[0] is input dim, dims[-1] is units in hidden layer.
+#             The decoder is symmetric with encoder. So number of layers of the auto-encoder is 2*len(dims)-1
+#         act: activation, not applied to Input, Hidden and Output layers
+#     return:
+#         (ae_model, encoder_model), Model of autoencoder and model of encoder
+#     """
+#     n_stacks = len(dims) - 1
+#     # input
+#     input_img = Input(shape=(dims[0],), name='input')
+#     x = input_img
+#     # internal layers in encoder
+#     for i in range(n_stacks-1):
+#         x = Dense(dims[i + 1], activation=act, kernel_initializer=init, name='encoder_%d' % i)(x)
 
-    # hidden layer
-    encoded = Dense(dims[-1], kernel_initializer=init, name='encoder_%d' % (n_stacks - 1))(x)  # hidden layer, features are extracted from here
+#     # hidden layer
+#     encoded = Dense(dims[-1], kernel_initializer=init, name='encoder_%d' % (n_stacks - 1))(x)  # hidden layer, features are extracted from here
 
-    x = encoded
-    # internal layers in decoder
-    for i in range(n_stacks-1, 0, -1):
-        x = Dense(dims[i], activation=act, kernel_initializer=init, name='decoder_%d' % i)(x)
+#     x = encoded
+#     # internal layers in decoder
+#     for i in range(n_stacks-1, 0, -1):
+#         x = Dense(dims[i], activation=act, kernel_initializer=init, name='decoder_%d' % i)(x)
 
-    # output
-    x = Dense(dims[0], kernel_initializer=init, name='decoder_0')(x)
-    decoded = x
-    return Model(inputs=input_img, outputs=decoded, name='AE'), Model(inputs=input_img, outputs=encoded, name='encoder')
+#     # output
+#     x = Dense(dims[0], kernel_initializer=init, name='decoder_0')(x)
+#     decoded = x
+#     return Model(inputs=input_img, outputs=decoded, name='AE'), Model(inputs=input_img, outputs=encoded, name='encoder')
 
 
 autoencoder, encoder = autoencoder(dims, init=init)
