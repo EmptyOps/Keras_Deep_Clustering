365382cee67ff357b0bacd5399ebfb039ebeda39
hiren <hiren@emptyops.com>
2019-12-10 19:31:59 +0530

    adding data generator useful for large datasets, to be tested yet

--- DataGenerator.py
+++ DataGenerator.py
@@ -3,9 +3,9 @@ import keras
 
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
-    def __init__(self, list_IDs, labels, batch_size=32, dim=(32,32,32), n_channels=1,
-                 n_classes=10, shuffle=True, is_init_random_labels=False, datatype='.npy', datadirs=[], is_label_to_categorical=False):
-        'Initialization'
+    def __init__(self, list_IDs=None, labels=None, batch_size=32, dim=(32,32,32), n_channels=1,
+                 n_classes=10, shuffle=True, datatype='.npy', datadirs=[], is_label_to_categorical=False):
+        'Initialization need to provide either list_IDs or datadirs(dir paths)'
         self.dim = dim
         self.batch_size = batch_size
         self.n_channels = n_channels
@@ -17,14 +17,17 @@ class DataGenerator(keras.utils.Sequence):
 
 
         if not list_IDs == None:
+            self.is_dir_based_data_batches = False
             self.list_IDs = list_IDs
         else:
+            self.is_dir_based_data_batches = True
+            self.dir_batch_size = self.batch_size * 50
             self.__init_IDs__(list_IDs, datadirs)            
 
-        if is_init_random_labels == False:
+        if not labels == None:
             self.labels = labels
         else:
-            self.labels = self.init_random_labels(list_IDs, n_classes)
+            self.labels = self.init_random_labels( len(list_IDs), n_classes)
 
         self.on_epoch_end()
 
@@ -38,30 +41,64 @@ class DataGenerator(keras.utils.Sequence):
         self.datadirs_rec_count = []
 
         #count total
+        self.total_records = 0
         sdir = len(datadirs)
         for i in range(0, sdir):
             self.datadirs_rec_count[i] = len([True for name in os.listdir(self.datadirs[i]) if os.path.isfile(name)])
+            self.total_records = self.total_records + self.datadirs_rec_count[i]
 
-        #load first dir level batch
 
-
-    def load_dir_level_ID_batch(self, list_IDs, n_classes):
+    def load_dir_level_ID_batch(self, index):
         ''
+        tmp_cnt = 0
+        self.list_IDs = []
+        sdir = len(datadirs)
+        self.dir_batch_index = index
+        self.bstart = index * self.dir_batch_size
+        self.benddd = (index * self.dir_batch_size) + self.dir_batch_size
+        for i in range(0, sdir):
+            if tmp_cnt + self.datadirs_rec_count[i] >= self.bstart:
+                for name in os.listdir(self.datadirs[i]):
+                    if tmp_cnt >= self.bstart:
+                        if tmp_cnt < self.benddd:
+                            self.list_IDs.append( name )
+                        else:
+                            break
 
+                    tmp_cnt = tmp_cnt + 1
 
+            if tmp_cnt < self.benddd:
+                tmp_cnt = tmp_cnt + self.datadirs_rec_count[i] 
+            else:
+                break
 
-    def init_random_labels(self, list_IDs, n_classes):
+    def init_random_labels(self, list_IDs_len, n_classes):
         'usefull for unsupervised learning. class labels are generated in sequence for reproducibility'
+        return np.random.choice( list_IDs_len, n_classes )
 
 
     def __len__(self):
         'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.list_IDs) / self.batch_size))
+        if not self.is_dir_based_data_batches:
+            return int(np.floor(len(self.list_IDs) / self.batch_size))
+        else:
+            return int(np.floor(self.total_records / self.batch_size))
 
     def __getitem__(self, index):
         'Generate one batch of data'
+
+        #
+        if self.is_dir_based_data_batches:
+            if index*self.batch_size >= self.benddd:
+                self.load_dir_level_ID_batch(self.dir_batch_index + 1)    
+                self.indexes = np.arange(len(self.list_IDs))
+
+                if self.shuffle == True:
+                    np.random.shuffle(self.indexes)
+
+
         # Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        indexes = self.indexes[ np.mod(index*self.batch_size, self.dir_batch_size) : np.mod((index+1)*self.batch_size, self.dir_batch_size) ]
 
         # Find list of IDs
         list_IDs_temp = [self.list_IDs[k] for k in indexes]
@@ -73,11 +110,13 @@ class DataGenerator(keras.utils.Sequence):
 
     def on_epoch_end(self):
         'Updates indexes after each epoch'
-        if len(self.datadirs) <= 1:
+        if not self.is_dir_based_data_batches:
             self.indexes = np.arange(len(self.list_IDs))
         else:
-            self.indexes = np.arange(len(self.datadirs))
+            self.load_dir_level_ID_batch(0)
+            self.indexes = np.arange(len(self.list_IDs))
 
+        #TODO better to do dir array shuffle in case dir based batches
         if self.shuffle == True:
             np.random.shuffle(self.indexes)
 
@@ -90,7 +129,10 @@ class DataGenerator(keras.utils.Sequence):
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
             # Store sample
-            X[i,] = np.load('data/' + ID + self.datatype)
+            if self.datatype == '.npy':
+                X[i,] = np.load( ID )   #absolute or relative path to data file
+            elif self.datatype == 'imgs_to_gray':
+                X[i,] = cv2.cvtColor(cv2.imread( ID ), cv2.COLOR_BGR2GRAY)    #absolute or relative path to record file
 
             # Store class
             y[i] = self.labels[ID]
